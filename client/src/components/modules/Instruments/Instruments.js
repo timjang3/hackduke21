6ba91ae3b601db25b53a76cdaccbead18ab4d24c
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2021-10-23 21:22:56 -0400

    Added home page

---
+++ client/src/components/modules/Instruments/Instruments.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import * as Styled from "./Instruments.styled";
+
+export function Instruments() {
+  return (
+    <Styled.Background>
+      This is the instruments component
+    </Styled.Background>
+  );
+}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,19 +1,29 @@
-import { TopBar } from "../components/modules/index";
-import { NextSeo } from "next-seo";
-import { HomeBanner } from "../../styles/ConstantStyles";
-import styled from "styled-components";
-import { BiRightArrowAlt } from "react-icons/bi";
+
 import { keyframes } from "styled-components";
 import { useRouter } from "next/router";
+import {TopBar, Landing, Instruments} from "../components/modules/index"
 import { useMediaQuery } from "../hooks/useMediaQuery";
+import styled from "styled-components"
 
-export default function Home({ data }) {
-  let isBuilderFriendly = useMediaQuery("(min-width: 1310px)");
+export default function Home() {
   const router = useRouter();
   return (
     <div>
-      <NextSeo title="HackDuke 21" description="" />
       <TopBar />
+      <Landing />
+      <Flex>
+        <Instruments />
+        <Instruments />
+        <Instruments />
+      </Flex>
     </div>
   );
 }
+
+export const Flex = styled.div`
+  width:1200px;
+  margin:auto;
+  display:flex;
+  margin-top:30px;
+  justify-content:space-between;
+`
